f68dc99aec8355faf02aa5c047c9627d3dc8afb0
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-03-18 00:13:01 +0300

    navbar+component

---
+++ nexus/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+    <div>
+        <div>
+            <img src={assets.logo} alt="" />
+        </div>
+    </div>
+}
+
+export default Navbar
